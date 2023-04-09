2b07044d00c7100c5193746fe541b0fe0289c422
Kola Kolade <kolakolade@gmail.com>
2023-04-09 01:35:37 +0100

    implement asyncThunk, useDispatch and useSelector

--- src/components/FetchedUsers.jsx
+++ src/components/FetchedUsers.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store/users/usersSlice';
 
 const FetchedUsers = () => {
-  const { users, isLoading, error } = useSelector((store) => store.users.users);
-  console.log(users);
+  const users = useSelector((state) => state.users);
+  const isLoading = useSelector((state) => state.isLoading);
+  const error = useSelector((state) => state.error);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers);
+  }, [dispatch]);
+
   if (error) return <p>Cannot fetch users</p>;
 
   if (isLoading) return <p>Loading ...</p>;
 
   return (
     <div>
-      {users.results.map((user, index) => (
+      {users.map((user, index) => (
         <li key={index}>
           {user.name.first} {user.name.last}
         </li>
--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,4 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', () => {
+  try {
+    const { data } = axios.get('https://randomuser.me/api/?results=5');
+    console.log(data);
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const initialState = {
   users: [],
@@ -10,7 +21,19 @@ export const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state) => {
+      state.isLoading = false;
+      state.error = true;
+    });
+  },
 });
 
 export default usersSlice.reducer;
